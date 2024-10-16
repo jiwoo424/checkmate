da1031cc7b15caf6da734bec1fc37717d8a780e3
jiwoo424 <jiwoomail@naver.com>
2024-10-16 20:14:58 +0900

    commit

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ elif st.session_state["current_page"] == "upload":
             clauses = st.session_state["ocr_result"]["clauses"]
             detection_results = st.session_state["ocr_result"]["detection_results"]
 
-        st.subheader("<p>총 {total_clauses}개의 조항 중 <b style='color:red'>{num_risky}</b>개의 위험 조항이 감지되었습니다.</p>")
+        st.subheader("총 {total_clauses}개의 조항 중 {num_risky}개의 위험 조항이 감지되었습니다.")
         st.divider()
 
         for i, clause in enumerate(clauses):
