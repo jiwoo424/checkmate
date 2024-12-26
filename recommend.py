105e563891b19f9f717d63f308c1b04cd6e4de94
jiwoo424 <jiwoomail@naver.com>
2024-12-26 18:42:52 +0900

    commit

--- recommend.py
+++ recommend.py
@@ -1,13 +1,16 @@
 import numpy as np
+import streamlit as st
 from langchain_upstage import UpstageEmbeddings
 
+api_key = st.secrets['API_KEY']
+
 def get_embedding(text):
-    embeddings = UpstageEmbeddings(api_key=os.environ["API_KEY"], model="solar-embedding-1-large-passage")
+    embeddings = UpstageEmbeddings(api_key=api_key, model="solar-embedding-1-large-passage")
     response = embeddings.embed_documents([text])  # Upstage는 리스트 형식 입력 요구
     return np.array(response[0]) 
 
 
-def recommend(clause, df, threshold=0.4):
+def recommend_clause(clause, df, threshold=0.4):
     embedded_clause = get_embedding(clause)
     embeddings = df['feature'].values
     distances = [np.linalg.norm(embedded_clause - emb) for emb in embeddings]
